1a6ab906ae76ac8b4622779419e0d94581d813d3
Nuha-hub <134822512+Nuha-hub@users.noreply.github.com>
2024-07-16 15:39:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,67 +1,55 @@
-import requests,random,secrets
-from user_agent import generate_user_agent
+import requests,re,random,time
 from flask import Flask, jsonify, request
+from user_agent import *
+
+
+
+
+
 app = Flask(__name__)
 
-@app.route('/search/id=<re>/by/cc_02', methods=['GET'])
-def search(re):
-	try:
-		csrftoken = secrets.token_hex(32)
-		mid=secrets.token_hex(27)
-		ig_=secrets.token_hex(36)
-		datr=secrets.token_hex(24)
-		app=''.join(random.choice('936619743392459')for i in range(15))
-		cookies = {
-			'csrftoken': csrftoken,
-			'ps_l': '0',
-			'ps_n': '0',
-			'ig_did': f'{ig_}',
-			'ig_nrcb': '1',
-			'dpr': '2.1988937854766846',
-			'mid': mid,
-			'datr': datr,
-}
-		headers = {
-			'sec-fetch-site': 'same-origin',
-			'viewport-width': '891',
-			'x-asbd-id': '129477',
-			'user-agent':generate_user_agent(),
-			'x-csrftoken': csrftoken,
-			'x-ig-app-id': app,
-			'x-fb-friendly-name': 'PolarisProfilePageContentQuery',
-			'x-fb-lsd': 'AVoRhvRPoRs',
-}
-
-
-		data = {
-			'csr': 'gVb2snsIjkIQyjRmBaFGECih59Fb98nQBzbZ2IN8BqBGl7h9Am4ohAAD-vGBh4GizA-4aAiJ2vFDUR3qx596AhrBgzJlBKmu6VHiypryUkByrGiicgPAx6iUpGEOmqfykFA4801kXEkOwmU1Tqwvk8wCix64E0b_EaWdguwozat2F61-wiokxG0d9w2MFU5Kzo0k6wiU7Kut2F601_Ew1me',
-			'lsd': 'AVoRhvRPoRs',
-			'variables': '{"id":"'+re+'"}',
-			'server_timestamps': 'true',
-			'doc_id': '7381344031985950',
-}
-
-		response = requests.post('https://www.instagram.com/api/graphql',cookies=cookies,  headers=headers, data=data).json()
-		full_name = response['data']['user']['full_name']
-		username = response['data']['user']['username']
-		is_private = response['data']['user']['is_private']
-		biography = response['data']['user']['biography']
-		follower_count= response['data']['user']['follower_count']
-		following_count= response['data']['user']['following_count']
-		media_count = response['data']['user']['media_count']
-		profile_pic_url = response['data']['user']['hd_profile_pic_url_info']['url']
+@app.route('/check/email=<email>/by/cc_02', methods=['GET'])
+def chk(email):
+	ma = requests.Session()
+	passwor = f"#PWD_INSTAGRAM_BROWSER:0:{int(time.time())}:hassan11inthetop878n"
+	s3 = ma.get('https://www.instagram.com/accounts/login/').cookies.get_dict()['csrftokrn']
+	rs3 = ma.get('https://www.instagram.com/accounts/login/')
+	ctk = rs3.text.replace("\\", "").split('csrf_token\":\"')[1].split('"')[0]
+	headers = {
+	            "user-agent": generate_user_agent(),
+	            "x-csrftoken": ctk,
+	            "x-ig-www-claim": "0",
+	        }
+	rs3 = ma.post(
+	            "https://www.instagram.com/api/v1/web/accounts/login/ajax/",
+	            headers=headers,
+	data={
+	                "enc_password": passwor,
+	                "username": email,
+	                "queryParams": "{}",
+	                "optIntoOneTap": "false",
+	                "trustedDeviceRecords": "{}"
+	            },
+	        )
+	
+	headers.update({"x-ig-set-www-claim":"0"})
+	headers.update({"x-csrftoken": ctk})
+	if 'html' in rs3.text:
 		return jsonify({
-		'username':username,
-		'full_name':full_name,
-		'is_private':is_private,
-		'profile_pic_url':profile_pic_url,
-		'biography':biography, 'follower':follower_count,
-		'following':following_count,
-		'posts':media_count
+			'by':'@cc_02',
+			'email':f'{email}',
+			'message':'Available iG',
+			'status':True
 		})
-		
-	except:
-		return {'status':'bad','message':'The Id Is Invaild Or Send request After 3 second '}
+	else:
+		return jsonify({
+			'by':'@cc_02',
+			'email':f'{email}',
+			'message':'unAvailable iG',
+			'status':False
+		})
+
+
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
